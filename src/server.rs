bb3a455d601faea2d2d32eb7f950e0038351e0ef
Tibor Kircsi <tkircsi@gmail.com>
2023-02-22 00:31:46 +0100

    add custom ParseError

--- src/server.rs
+++ src/server.rs
@@ -1,4 +1,7 @@
 use std::net::TcpListener;
+use crate::http::Request;
+use std::convert::TryFrom;
+use std::io::Read;
 
 pub struct Server {
     addr: String,
@@ -15,11 +18,27 @@ impl Server {
 
         loop {
             match listener.accept() {
-                Ok((stream, address)) => {}
+                Ok((mut stream, address)) => {
+                    let mut buffer = [0; 1024];
+                    match stream.read(&mut buffer) {
+                        Ok(_) => {
+                            println!("Received a request: {}", String::from_utf8_lossy(&buffer));
+                            let res: Result<Request, String> = Request::try_from(&buffer[..]);
+                            match res {
+                                Ok(request) => {},
+                                Err(e) => println!("failed to parse a request: {}", e),
+                            }
+                            // let res: Result<Request, String> = &buffer[..].try_into();
+                        },
+                        Err(e) => {
+                            println!("failed to read from connection: {}", e);
+                        },
+                    }
+                },
                 Err(e) => {
                     println!("connection error: {}", e);
                     continue;
-                }
+                },
             };
         }
     }
