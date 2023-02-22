37992c7c13e3cf15a6744e5e14141d828d2aad46
Tibor Kircsi <tkircsi@gmail.com>
2023-02-22 18:49:29 +0100

    parse method

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,4 +1,4 @@
-use super::method::Method;
+use super::method::{Method, MethodError};
 use std::convert::TryFrom;
 use std::error::Error;
 use std::fmt::{Debug, Display, Formatter, Result as FmtResult};
@@ -50,7 +50,14 @@ impl TryFrom<&[u8]> for Request {
         if protocol != "HTTP/1.1" {
             return Err(ParseError::InvalidProtocol);
         }
-        Ok(Request {})
+
+        let method: Method = method.parse()?;
+
+        Ok(Request {
+            method,
+            path: "path".to_string(),
+            query_string: None,
+        })
     }
 }
 
@@ -94,8 +101,14 @@ impl Debug for ParseError {
 }
 
 impl From<Utf8Error> for ParseError {
-    fn from(value: Utf8Error) -> Self {
+    fn from(_: Utf8Error) -> Self {
         Self::InvalidEncoding
+    }
+}
+
+impl From<MethodError> for ParseError {
+    fn from(_: MethodError) -> Self {
+        Self::InvalidMethod
     }
 }
 
