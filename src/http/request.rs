237ccd84aa0d54ba05d5df73cd1d3892e81f5c0c
Tibor Kircsi <tkircsi@gmail.com>
2023-02-22 17:32:36 +0100

    parsing request

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,7 +1,8 @@
 use super::method::Method;
 use std::convert::TryFrom;
 use std::error::Error;
-use std::fmt::{Display, Formatter, Result as FmtResult, Debug};
+use std::fmt::{Debug, Display, Formatter, Result as FmtResult};
+use std::str::{self, Utf8Error};
 
 pub struct Request {
     path: String,
@@ -12,9 +13,54 @@ pub struct Request {
 impl TryFrom<&[u8]> for Request {
     type Error = ParseError;
 
-    fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
-        unimplemented!()
+    fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
+        /* Solution 1:
+            match str::from_utf8(buf) {
+            Ok(req) => {}
+            Err(_) => return Err(ParseError::InvalidEncoding),
+        }
+        */
+
+        /* Solution 2:
+        match str::from_utf8(buf).or(Err(ParseError::InvalidEncoding)) {
+            Ok(req) => {}
+            Err(e) => Err(e),
+        }
+        */
+
+        // Solution 3:
+        // let req = str::from_utf8(buf).or(Err(ParseError::InvalidEncoding))?;
+
+        // Solution 4:
+        // Implemented From trait for ParseError for fconversion.
+        let req = str::from_utf8(buf)?;
+
+        /* Solution 1:
+        match get_next_word(req) {
+            Some((method, req)) => {}
+            None => return Err(ParseError::InvalidRequest),
+        }
+        */
+
+        // Solution 2
+        let (method, req) = get_next_word(req).ok_or(ParseError::InvalidRequest)?;
+        let (path, req) = get_next_word(req).ok_or(ParseError::InvalidRequest)?;
+        let (protocol, _) = get_next_word(req).ok_or(ParseError::InvalidRequest)?;
+
+        if protocol != "HTTP/1.1" {
+            return Err(ParseError::InvalidProtocol);
+        }
+        Ok(Request {})
     }
+}
+
+fn get_next_word(string: &str) -> Option<(&str, &str)> {
+    for (i, c) in string.char_indices() {
+        if c == ' ' || c == '\r' {
+            return Some((&string[..i], &string[i + 1..]));
+        }
+    }
+    None
 }
 
 pub enum ParseError {
@@ -27,13 +73,13 @@ pub enum ParseError {
 impl ParseError {
     fn message(&self) -> &str {
         match self {
-           Self::InvalidRequest => "Invalid Request",
-           Self::InvalidEncoding => "Invalid Encoding",
-           Self::InvalidProtocol => "Invalid Protocol",
-           Self::InvalidMethod => "Invalid Method",
+            Self::InvalidRequest => "Invalid Request",
+            Self::InvalidEncoding => "Invalid Encoding",
+            Self::InvalidProtocol => "Invalid Protocol",
+            Self::InvalidMethod => "Invalid Method",
         }
     }
- }
+}
 
 impl Display for ParseError {
     fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
@@ -47,5 +93,10 @@ impl Debug for ParseError {
     }
 }
 
-impl Error for ParseError {}
+impl From<Utf8Error> for ParseError {
+    fn from(value: Utf8Error) -> Self {
+        Self::InvalidEncoding
+    }
+}
 
+impl Error for ParseError {}
--- src/server.rs
+++ src/server.rs
@@ -1,7 +1,7 @@
-use std::net::TcpListener;
-use crate::http::Request;
+use crate::http::{ParseError, Request};
 use std::convert::TryFrom;
 use std::io::Read;
+use std::net::TcpListener;
 
 pub struct Server {
     addr: String,
@@ -23,22 +23,22 @@ impl Server {
                     match stream.read(&mut buffer) {
                         Ok(_) => {
                             println!("Received a request: {}", String::from_utf8_lossy(&buffer));
-                            let res: Result<Request, String> = Request::try_from(&buffer[..]);
+                            let res: Result<Request, ParseError> = Request::try_from(&buffer[..]);
                             match res {
-                                Ok(request) => {},
+                                Ok(request) => {}
                                 Err(e) => println!("failed to parse a request: {}", e),
                             }
                             // let res: Result<Request, String> = &buffer[..].try_into();
-                        },
+                        }
                         Err(e) => {
                             println!("failed to read from connection: {}", e);
-                        },
+                        }
                     }
-                },
+                }
                 Err(e) => {
                     println!("connection error: {}", e);
                     continue;
-                },
+                }
             };
         }
     }
