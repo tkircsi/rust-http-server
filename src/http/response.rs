d43fc77643c96592b9fb12c8c5c745b412bfc5df
Tibor Kircsi <tkircsi@gmail.com>
2023-02-24 22:35:51 +0100

    implement send method for Response to save heap

--- src/http/response.rs
+++ src/http/response.rs
@@ -1,4 +1,9 @@
 use super::status_code::SatusCode;
+use std::{
+    fmt::{Display, Formatter, Result as FmtResult},
+    io::{Result as IoResult, Write},
+    net::TcpStream,
+};
 
 #[derive(Debug)]
 pub struct Response {
@@ -10,4 +15,34 @@ impl Response {
     pub fn new(status_code: SatusCode, body: Option<String>) -> Self {
         Response { status_code, body }
     }
+
+    pub fn send(&self, stream: &mut TcpStream) -> IoResult<()> {
+        let body = match &self.body {
+            Some(b) => b,
+            None => "",
+        };
+        write!(
+            stream,
+            "HTTP/1.1 {} {}\r\n\r\n{}",
+            self.status_code,
+            self.status_code.reason_phrase(),
+            body,
+        )
+    }
 }
+
+/* impl Display for Response {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        let body = match &self.body {
+            Some(b) => b,
+            None => "",
+        };
+        write!(
+            f,
+            "HTTP/1.1 {} {}\r\n\r\n{}",
+            self.status_code,
+            self.status_code.reason_phrase(),
+            body,
+        )
+    }
+} */
--- src/server.rs
+++ src/server.rs
@@ -1,6 +1,6 @@
-use crate::http::{ParseError, Request};
+use crate::http::{response, ParseError, Request, Response, SatusCode};
 use std::convert::TryFrom;
-use std::io::Read;
+use std::io::{Read, Write};
 use std::net::TcpListener;
 
 pub struct Server {
@@ -25,11 +25,26 @@ impl Server {
                         Ok(_) => {
                             println!("Received a request: {}", String::from_utf8_lossy(&buffer));
                             let res: Result<Request, ParseError> = Request::try_from(&buffer[..]);
-                            match res {
+                            let response = match res {
                                 Ok(request) => {
                                     dbg!(request);
+                                    Response::new(
+                                        SatusCode::Ok,
+                                        Some("<h1>Hurray!!!</h1>".to_string()),
+                                    )
+
+                                    // Implement it in Response.send to avoid to copy the entire Response
+                                    // on the heap.
+                                    // write!(stream, "{}", response);
                                 }
-                                Err(e) => println!("failed to parse a request: {}", e),
+                                Err(e) => {
+                                    println!("failed to parse a request: {}", e);
+                                    Response::new(SatusCode::BadRequest, None)
+                                }
+                            };
+
+                            if let Err(e) = response.send(&mut stream) {
+                                println!("failed to send response: {}", e);
                             }
                             // let res: Result<Request, String> = &buffer[..].try_into();
                         }
