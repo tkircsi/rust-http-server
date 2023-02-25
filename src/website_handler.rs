413200a6703487ca46a70849a82faefbd190ff87
Tibor Kircsi <tkircsi@gmail.com>
2023-02-25 19:29:43 +0100

    serve static files

--- src/website_handler.rs
+++ src/website_handler.rs
@@ -1,5 +1,6 @@
 use super::http::{Method, ParseError, Request, Response, SatusCode};
 use super::server::Handler;
+use std::fs;
 
 pub struct WebsiteHandler {
     public_path: String,
@@ -9,16 +10,34 @@ impl WebsiteHandler {
     pub fn new(public_path: String) -> Self {
         Self { public_path }
     }
+
+    pub fn read_file(&self, file_path: &str) -> Option<String> {
+        let path = format!("{}/{}", self.public_path, file_path);
+        match fs::canonicalize(path) {
+            Ok(path) => {
+                if path.starts_with(self.public_path) {
+                    fs::read_to_string(path).ok()
+                } else {
+                    println!("directory traversal attack detected: {}", path);
+                    None
+                }
+            }
+            Err(_) => None,
+        }
+    }
 }
 
 impl Handler for WebsiteHandler {
     fn handle_request(&mut self, request: &Request) -> Response {
         match request.method() {
             Method::GET => match request.path() {
-                "/" => Response::new(SatusCode::Ok, Some("<h1Welcome!</h1>".to_string())),
-                "/hello" => Response::new(SatusCode::Ok, Some("<h1>Hello!</h1>".to_string())),
+                "/" => Response::new(SatusCode::Ok, self.read_file("index.html")),
+                "/hello" => Response::new(SatusCode::Ok, self.read_file("hello.html")),
                 "/bad" => Response::new(SatusCode::BadRequest, None),
-                _ => Response::new(SatusCode::NotFound, None),
+                path => match self.read_file(path) {
+                    Some(content) => Response::new(SatusCode::Ok, Some(content)),
+                    None => Response::new(SatusCode::NotFound, None),
+                },
             },
             _ => Response::new(SatusCode::NotFound, None),
         }
