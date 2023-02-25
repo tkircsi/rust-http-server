64156bbba4339839160c5f3ad403f03e8433e6a4
Tibor Kircsi <tkircsi@gmail.com>
2023-02-25 17:39:37 +0100

    add public_path and env vars

--- src/website_handler.rs
+++ src/website_handler.rs
@@ -1,14 +1,27 @@
-use super::http::{ParseError, Request, Response, SatusCode};
+use super::http::{Method, ParseError, Request, Response, SatusCode};
 use super::server::Handler;
 
-pub struct WebsiteHandler {}
+pub struct WebsiteHandler {
+    public_path: String,
+}
+
+impl WebsiteHandler {
+    pub fn new(public_path: String) -> Self {
+        Self { public_path }
+    }
+}
 
 impl Handler for WebsiteHandler {
     fn handle_request(&mut self, request: &Request) -> Response {
-        Response::new(
-            SatusCode::Ok,
-            Some("<h1>Website Handler test</h1>".to_string()),
-        )
+        match request.method() {
+            Method::GET => match request.path() {
+                "/" => Response::new(SatusCode::Ok, Some("<h1Welcome!</h1>".to_string())),
+                "/hello" => Response::new(SatusCode::Ok, Some("<h1>Hello!</h1>".to_string())),
+                "/bad" => Response::new(SatusCode::BadRequest, None),
+                _ => Response::new(SatusCode::NotFound, None),
+            },
+            _ => Response::new(SatusCode::NotFound, None),
+        }
     }
 
     fn handle_bad_request(&mut self, e: &ParseError) -> Response {
